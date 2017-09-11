81bc1c94a164628481cd4c7249c22c0a28a5223c
zzy <891177434@qq.com>
2017-09-11 09:20:24 +0800

    不断的完善自己的gulpfile.js文件首次提交

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var less        = require('gulp-less');
+var reload      = browserSync.reload;
+// 错误阻止
+
+var plumber = require('gulp-plumber');
+
+// 静态服务器 + 监听 scss/html 文件
+gulp.task('serve', ['less'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./less/*.less", ['less']);
+    gulp.watch("./*.html").on('change', reload);
+});
+
+// scss编译后的css将注入到浏览器里实现更新
+gulp.task('less', function() {
+    return gulp.src("./less/*.less")
+    	.pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest("./css/"))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['serve']);
